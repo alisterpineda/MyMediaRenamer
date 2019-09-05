a0ca6852ca9bb2c01a440eb6026adc5116691a57
Alister Pineda <alister@alisterpineda.com>
2019-09-04 20:08:11 -0600

    Add new testcase called "SingleTagTest" in PatternParserTests.cs Tests for tag patterns incl. all their associated options.

--- MyMediaRenamer.Tests/Core/PatternParserTests.cs
+++ MyMediaRenamer.Tests/Core/PatternParserTests.cs
@@ -8,6 +8,31 @@ namespace MyMediaRenamer.Tests.Core
 {
     public class PatternParserTests : BaseTestFixture
     {
+        public static object[] SingleTagCases =
+        {
+            new object[] {"<datetime>", new DateTimeTag(),  },
+            new object[] { "<datetime;format=yyyyMMdd_HHmmss_fff>", new DateTimeTag{Format = "yyyyMMdd_HHmmss_fff" } },
+            new object[] {"<hash>", new HashTag() },
+            new object[] {"<hash;algorithm=md5>", new HashTag() },
+            new object[] {"<hash;algorithm=sha1>", new HashTag{Algorithm = Algorithm.SHA1} },
+            new object[] {"<hash;algorithm=sha256>", new HashTag{Algorithm = Algorithm.SHA256} },
+            new object[] {"<hash;algorithm=sha384>", new HashTag{Algorithm = Algorithm.SHA384} },
+            new object[] {"<hash;algorithm=sha512>", new HashTag{Algorithm = Algorithm.SHA512} },
+            new object[] {"<name>", new NameTag() },
+            new object[] {"<name;type=name>", new NameTag() },
+            new object[] {"<name;type=extension>", new NameTag{Type = NameTagType.Extension} },
+            new object[] {"<name;type=full>", new NameTag{Type = NameTagType.Full} },
+        };
+
+        [TestCaseSource(nameof(SingleTagCases))]
+        public void SingleTagTest(string pattern, BaseTag expectedTag)
+        {
+            List<BaseTag> actualTags = PatternParser.Parse(pattern);
+            Assert.AreEqual(1, actualTags.Count);
+            Assert.AreEqual(expectedTag, actualTags[0]);
+
+        }
+
         public static object[] NTagsCases =
         {
             new object[] {"", new List<BaseTag>() },
