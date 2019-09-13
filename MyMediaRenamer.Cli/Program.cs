431c62425453f1399fa58af512d0b1c79b3cf7c3
Alister Pineda <alister@alisterpineda.com>
2019-09-12 21:10:13 -0600

    Add CLI project

---
+++ MyMediaRenamer.Cli/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace MyMediaRenamer.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.Name = "MyMediaRenamer";
+            app.Description = "hello world!";
+
+            app.HelpOption("-h|--h");
+
+            app.Execute(args);
+        }
+    }
+}
