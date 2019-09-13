cdc5acbf7428f966d65cac8355a25b060670874b
Alister Pineda <alister@alisterpineda.com>
2019-09-12 20:46:19 -0600

    Remove unused usings

--- MyMediaRenamer.Gui/Utilities/WindowService/RenamerSettingsDialogService.cs
+++ MyMediaRenamer.Gui/Utilities/WindowService/RenamerSettingsDialogService.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using MyMediaRenamer.Gui.ViewModels;
 using MyMediaRenamer.Gui.Views;
--- MyMediaRenamer.Tests/Core/RenamerTests.cs
+++ MyMediaRenamer.Tests/Core/RenamerTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Abstractions.TestingHelpers;
-using System.Text;
+﻿using System.Collections.Generic;
 using MyMediaRenamer.Core;
 using MyMediaRenamer.Core.FilePathTags;
 using NUnit.Framework;
