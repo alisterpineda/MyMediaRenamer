8eb981f14e13d6b30b9b958db5be15b018255aac
Alister Pineda <alister@alisterpineda.com>
2019-09-13 21:37:42 -0600

    Add lots of Exif SubIFD tags to MetadataTag

--- MyMediaRenamer.Core/FilePathTags/MetadataTag.cs
+++ MyMediaRenamer.Core/FilePathTags/MetadataTag.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using MetadataExtractor;
 using MetadataExtractor.Formats.Exif;
@@ -7,10 +8,45 @@ namespace MyMediaRenamer.Core.FilePathTags
 {
     public enum MetadataName
     {
+        // Exif IFD0
         ExifImageDescription,
         ExifMake,
         ExifModel,
         ExifOrientation,
+
+        // Exif SubIFD
+        ExifExposureTime,
+        ExifFNumber,
+        ExifExposureProgram,
+        ExifIso,
+        ExifVersion,
+        ExifDateTimeOriginal,
+        ExifDateTimeDigitized,
+        ExifComponentsConfiguration,
+        ExifShutterSpeed,
+        ExifAperture,
+        ExifBrightness,
+        ExifExposureBias,
+        ExifMeteringMode,
+        ExifFlash,
+        ExifFocalLength,
+        ExifSubjectLocation,
+        ExifSubSecTimeOriginal,
+        ExifSubSecTimeDigitized,
+        ExifColorSpace,
+        ExifImageWidth,
+        ExifImageHeight,
+        ExifSensingMethod,
+        ExifSceneType,
+        ExifExposureMode,
+        ExifWhiteBalanceMode,
+        ExifFocalLengthIn35,
+        ExifSceneCaptureType,
+        ExifLensSpecification,
+        ExifLensMake,
+        ExifLensModel,
+
+        // Exif GPS
         GpsLatitudeRef,
         GpsLatitude,
         GpsLongitudeRef,
@@ -22,18 +58,51 @@ namespace MyMediaRenamer.Core.FilePathTags
     public class MetadataTag : BaseTag
     {
         #region Members
-        private static readonly Dictionary<MetadataName, int> _enumToTagTypeMap = new Dictionary<MetadataName, int>
+        private static readonly Dictionary<MetadataName, (Type, int)> _enumToTagTypeMap = new Dictionary<MetadataName, (Type,int)>
         {
-            { MetadataName.ExifImageDescription, ExifDirectoryBase.TagImageDescription },
-            { MetadataName.ExifMake, ExifDirectoryBase.TagMake },
-            { MetadataName.ExifModel, ExifDirectoryBase.TagModel },
-            { MetadataName.ExifOrientation, ExifDirectoryBase.TagOrientation },
-            { MetadataName.GpsLatitudeRef, GpsDirectory.TagLatitudeRef },
-            { MetadataName.GpsLatitude, GpsDirectory.TagLatitude },
-            { MetadataName.GpsLongitudeRef, GpsDirectory.TagLongitudeRef },
-            { MetadataName.GpsLongitude, GpsDirectory.TagLongitude },
-            { MetadataName.GpsAltitudeRef, GpsDirectory.TagAltitudeRef },
-            { MetadataName.GpsAltitude, GpsDirectory.TagAltitude }
+            // Exif IFD0
+            { MetadataName.ExifImageDescription, (typeof(ExifIfd0Directory), ExifDirectoryBase.TagImageDescription) },
+            { MetadataName.ExifMake, (typeof(ExifIfd0Directory), ExifDirectoryBase.TagMake) },
+            { MetadataName.ExifModel, (typeof(ExifIfd0Directory), ExifDirectoryBase.TagModel) },
+            { MetadataName.ExifOrientation, (typeof(ExifIfd0Directory), ExifDirectoryBase.TagOrientation) },
+            // Exif SubIFD
+            { MetadataName.ExifExposureTime, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagExposureTime) },
+            { MetadataName.ExifFNumber, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagFNumber) },
+            { MetadataName.ExifExposureProgram, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagExposureProgram) },
+            { MetadataName.ExifIso, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagIsoEquivalent) },
+            { MetadataName.ExifVersion, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagExifVersion) },
+            { MetadataName.ExifDateTimeOriginal, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagDateTimeOriginal) },
+            { MetadataName.ExifDateTimeDigitized, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagDateTimeDigitized) },
+            { MetadataName.ExifComponentsConfiguration, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagComponentsConfiguration) },
+            { MetadataName.ExifShutterSpeed, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagShutterSpeed) },
+            { MetadataName.ExifAperture, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagAperture) },
+            { MetadataName.ExifBrightness, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagBrightnessValue) },
+            { MetadataName.ExifExposureBias, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagExposureBias) },
+            { MetadataName.ExifMeteringMode, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagMeteringMode) },
+            { MetadataName.ExifFlash, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagFlash) },
+            { MetadataName.ExifFocalLength, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagFocalLength) },
+            { MetadataName.ExifSubjectLocation, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagSubjectLocation) },
+            { MetadataName.ExifSubSecTimeOriginal, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagSubsecondTimeOriginal) },
+            { MetadataName.ExifSubSecTimeDigitized, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagSubsecondTimeDigitized) },
+            { MetadataName.ExifColorSpace, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagColorSpace) },
+            { MetadataName.ExifImageWidth, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagExifImageWidth) },
+            { MetadataName.ExifImageHeight, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagExifImageHeight) },
+            { MetadataName.ExifSensingMethod, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagSensingMethod) },
+            { MetadataName.ExifSceneType, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagSceneType) },
+            { MetadataName.ExifExposureMode, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagExposureMode) },
+            { MetadataName.ExifWhiteBalanceMode, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagWhiteBalanceMode) },
+            { MetadataName.ExifFocalLengthIn35, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.Tag35MMFilmEquivFocalLength) },
+            { MetadataName.ExifSceneCaptureType, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagSceneCaptureType) },
+            { MetadataName.ExifLensSpecification, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagLensSpecification) },
+            { MetadataName.ExifLensMake, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagLensMake) },
+            { MetadataName.ExifLensModel, (typeof(ExifSubIfdDirectory), ExifDirectoryBase.TagLensModel) },
+            // Exif GPS
+            { MetadataName.GpsLatitudeRef, (typeof(GpsDirectory),GpsDirectory.TagLatitudeRef) },
+            { MetadataName.GpsLatitude, (typeof(GpsDirectory), GpsDirectory.TagLatitude) },
+            { MetadataName.GpsLongitudeRef, (typeof(GpsDirectory), GpsDirectory.TagLongitudeRef) },
+            { MetadataName.GpsLongitude, (typeof(GpsDirectory), GpsDirectory.TagLongitude) },
+            { MetadataName.GpsAltitudeRef, (typeof(GpsDirectory), GpsDirectory.TagAltitudeRef) },
+            { MetadataName.GpsAltitude, (typeof(GpsDirectory), GpsDirectory.TagAltitude) }
         };
         #endregion
 
@@ -78,16 +147,15 @@ namespace MyMediaRenamer.Core.FilePathTags
 
         protected override string GenerateString(Renamer renamer, MediaFile mediaFile)
         {
-            int tagType = _enumToTagTypeMap[Name];
+            (Type directoryType, int tagType) = _enumToTagTypeMap[Name];
+            var metadataDirectory = mediaFile.MetadataDirectories.FirstOrDefault(x => x.GetType() == directoryType);
 
             if (UseRaw)
             {
-                return mediaFile.MetadataDirectories.Where(x => x.ContainsTag(tagType))
-                    ?.FirstOrDefault()?.GetString(tagType);
+                return metadataDirectory?.GetString(tagType);
             }
 
-            return mediaFile.MetadataDirectories.Where(x => x.ContainsTag(tagType))
-                ?.FirstOrDefault()?.GetDescription(tagType);
+            return metadataDirectory?.GetDescription(tagType);
         }
 
         #endregion
--- MyMediaRenamer.Tests/Core/FilePathTags/MetadataTagTests.cs
+++ MyMediaRenamer.Tests/Core/FilePathTags/MetadataTagTests.cs
@@ -8,9 +8,52 @@ namespace MyMediaRenamer.Tests.Core.FilePathTags
     {
         private static object[] GetStringCases =
         {
+            //-- Functionality tests --//
             new object[] { IosPhoto, new MetadataTag(), null },
             new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifOrientation}, "Top, left side (Horizontal / normal)" },
             new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifOrientation, UseRaw = true}, "1" },
+
+            //-- Mapping Tests --//
+            // Exif IFD0
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifMake}, "Apple" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifModel}, "iPhone 7 Plus" },
+            // Exif SubIFD
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifExposureTime}, "1/349 sec" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifFNumber}, "f/2.8" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifExposureProgram}, "Program normal" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifIso}, "20" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifVersion}, "2.21" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifDateTimeOriginal}, "2017:08:26 16:00:19" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifDateTimeDigitized}, "2017:08:26 16:00:19" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifComponentsConfiguration}, "YCbCr" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifShutterSpeed}, "1/349 sec" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifBrightness}, "3607/391" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifExposureBias}, "0 EV" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifMeteringMode}, "Spot" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifFlash}, "Flash did not fire, auto" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifFocalLength}, "6.6 mm" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifSubjectLocation}, null }, // TODO MetadataExtractor :: TagSubjectLocation vs TagSubjectLocationTiffEp???            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifSubSecTimeOriginal}, "349" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifSubSecTimeDigitized}, "349" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifColorSpace}, "Undefined" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifImageWidth}, "4032 pixels" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifImageHeight}, "3024 pixels" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifSensingMethod}, "One-chip color area sensor" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifSceneType}, "Directly photographed image" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifExposureMode}, "Auto exposure" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifWhiteBalanceMode}, "Auto white balance" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifFocalLengthIn35}, "57 mm" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifSceneCaptureType}, "Standard" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifLensSpecification}, "3.99-6.6mm f/1.8-2.8" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifLensMake}, "Apple" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.ExifLensModel}, "iPhone 7 Plus back dual camera 6.6mm f/2.8" },
+
+            // Exif GPS
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.GpsLatitudeRef}, "N" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.GpsLatitude}, "12° 0' 16.6\"" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.GpsLongitudeRef}, "E" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.GpsLongitude}, "120° 12' 17.24\"" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.GpsAltitudeRef}, "Sea level" },
+            new object[] { IosPhoto, new MetadataTag{Name = MetadataName.GpsAltitude}, "200 metres" },
         };
 
         [TestCaseSource(nameof(GetStringCases))]
